daa9fe5945295983b879cfb0841749e087fc3b27
Tobías Curti <tobiascurti9@gmail.com>
2026-06-17 18:16:25 -0300

    Se agrega  boton y textbox

--- TP4_PuntoC_Ejercicio1/Form1.Designer.vb
+++ TP4_PuntoC_Ejercicio1/Form1.Designer.vb
@@ -22,10 +22,40 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        cmdEjecutar = New Button()
+        lstSalida = New ListBox()
+        SuspendLayout()
+        ' 
+        ' cmdEjecutar
+        ' 
+        cmdEjecutar.Location = New Point(350, 35)
+        cmdEjecutar.Name = "cmdEjecutar"
+        cmdEjecutar.Size = New Size(112, 34)
+        cmdEjecutar.TabIndex = 0
+        cmdEjecutar.Text = "Ejecutar"
+        cmdEjecutar.UseVisualStyleBackColor = True
+        ' 
+        ' lstSalida
+        ' 
+        lstSalida.FormattingEnabled = True
+        lstSalida.Location = New Point(222, 75)
+        lstSalida.Name = "lstSalida"
+        lstSalida.Size = New Size(369, 179)
+        lstSalida.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(lstSalida)
+        Controls.Add(cmdEjecutar)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents lstSalida As ListBox
 
 End Class
